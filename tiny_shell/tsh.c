bc85a4bbf783561e6f9d6cdf1c1f5b8df807dffb
miaodonghan <miaodonghan@gmail.com>
2013-10-10 01:19:48 -0400

    cd support, more cases pass

--- tiny_shell/tsh.c
+++ tiny_shell/tsh.c
@@ -28,87 +28,87 @@
  *    Milestone 1
  *
  ***************************************************************************/
-	#define __MYSS_IMPL__
+#define __MYSS_IMPL__
 
-  /************System include***********************************************/
-	#include <stdlib.h>
-	#include <signal.h>
-        #include <string.h>
+/************System include***********************************************/
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
 #include <unistd.h>    
 #include <stdio.h>
-  /************Private include**********************************************/
-	#include "tsh.h"
-	#include "io.h"
-	#include "interpreter.h"
-	#include "runtime.h"
+/************Private include**********************************************/
+#include "tsh.h"
+#include "io.h"
+#include "interpreter.h"
+#include "runtime.h"
 
-  /************Defines and Typedefs*****************************************/
-    /*  #defines and typedefs should have their names in all caps.
-     *  Global variables begin with g. Global constants with k. Local
-     *  variables should be in all lower case. When initializing
-     *  structures and arrays, line everything up in neat columns.
-     */
+/************Defines and Typedefs*****************************************/
+/*  #defines and typedefs should have their names in all caps.
+ *  Global variables begin with g. Global constants with k. Local
+ *  variables should be in all lower case. When initializing
+ *  structures and arrays, line everything up in neat columns.
+ */
 
-	#define BUFSIZE 80
+#define BUFSIZE 80
 
-  /************Global Variables*********************************************/
+/************Global Variables*********************************************/
 
-  /************Function Prototypes******************************************/
-	/* handles SIGINT and SIGSTOP signals */	
-	static void sig(int);
-	extern int fg_job;
-	extern char * fgCmd;    
-	extern bgjobL *bgjobs;
-  /************External Declaration*****************************************/
+/************Function Prototypes******************************************/
+/* handles SIGINT and SIGSTOP signals */	
+static void sig(int);
+extern int fg_job;
+extern char * fgCmd;    
+extern bgjobL *bgjobs;
+/************External Declaration*****************************************/
 
 /**************Implementation***********************************************/
 
 int main (int argc, char *argv[])
 {
-	/* Initialize command buffer */
-	char* cmdLine = malloc(sizeof(char*)*BUFSIZE);
+    /* Initialize command buffer */
+    char* cmdLine = malloc(sizeof(char*)*BUFSIZE);
 
-	/* shell initialization */
-	if (signal(SIGINT, sig) == SIG_ERR) PrintPError("SIGINT");
-	if (signal(SIGTSTP, sig) == SIG_ERR) PrintPError("SIGTSTP");
-	if (signal(SIGCHLD, sig) == SIG_ERR) PrintPError("SIGCHLD");
+    /* shell initialization */
+    if (signal(SIGINT, sig) == SIG_ERR) PrintPError("SIGINT");
+    if (signal(SIGTSTP, sig) == SIG_ERR) PrintPError("SIGTSTP");
+    if (signal(SIGCHLD, sig) == SIG_ERR) PrintPError("SIGCHLD");
 
-	while (!forceExit) /* repeat forever */
-	{
-		/* read command line */
-		getCommandLine(&cmdLine, BUFSIZE);
+    while (!forceExit) /* repeat forever */
+    {
+        /* read command line */
+        getCommandLine(&cmdLine, BUFSIZE);
 
-		if(strcmp(cmdLine, "exit") == 0)
-		{
-			forceExit=TRUE;
-			continue;
-		}
+        if(strcmp(cmdLine, "exit") == 0)
+        {
+            forceExit=TRUE;
+            continue;
+        }
 
-		/* checks the status of background jobs */
-		//CheckJobs();
+        /* checks the status of background jobs */
+        CheckJobs();
 
-		/* interpret command and line
-		 * includes executing of commands */
-		Interpret(cmdLine);
+        /* interpret command and line
+         * includes executing of commands */
+        Interpret(cmdLine);
 
-	}
-	if(bgjobs != NULL){
-		bgjobL * p = bgjobs;
-		bgjobL * tmp;
-		while(p!=NULL){
-			tmp = p->next;
-			//if(p->state == RUNNING)
-			//	kill(-p->pid,SIGINT);
-			free(p->cmd);
-			free(p);
-			p = tmp;    
-		}    
-	}
-	/* shell termination */
-	free(cmdLine);
-	fflush(stdout);
-	//sleep(1);
-	return 0;
+    }
+    if(bgjobs != NULL){
+        bgjobL * p = bgjobs;
+        bgjobL * tmp;
+        while(p!=NULL){
+            tmp = p->next;
+            //if(p->state == RUNNING)
+            //	kill(-p->pid,SIGINT);
+            free(p->cmd);
+            free(p);
+            p = tmp;    
+        }    
+    }
+    /* shell termination */
+    free(cmdLine);
+    fflush(stdout);
+    //sleep(1);
+    return 0;
 } /* end main */
 
 static void sig(int signo)
@@ -126,65 +126,65 @@ static void sig(int signo)
         if(fg_job){
             kill(-fg_job,SIGTSTP);
             bgjobL *p = (bgjobL *)malloc(sizeof(bgjobL));
-		/*
-            int size = strlen(p->cmdline);
-		    char * pa = malloc(size+2);
- 		    strcpy(pa, cmd->cmdline);
-	            pa[size] = '&';
-                    pa[size+1] ='\0';
-		    bgjob -> cmd = pa;
-	    */
-	    p->cmd = fgCmd;
-	    p->pid = fg_job;
+            /*
+               int size = strlen(p->cmdline);
+               char * pa = malloc(size+2);
+               strcpy(pa, cmd->cmdline);
+               pa[size] = '&';
+               pa[size+1] ='\0';
+               bgjob -> cmd = pa;
+             */
+            p->cmd = fgCmd;
+            p->pid = fg_job;
             p->state = STOPPED;
             AddToBgJobs(p);
             //printf("[%i]   Stopped. \n", fg_job);
             //fflush(stdout);
             fg_job = 0;
-	    fgCmd = NULL;
+            fgCmd = NULL;
         }
         return;
     }           
-    
+
     if(signo == SIGCHLD){
-	    pid_t pid = 1;
-	    int status;
-	    while(  pid >0 ){
-		    pid = waitpid(-1, &status, WNOHANG );
-		    if(WIFEXITED(status)){
-			    if (pid == fg_job){ 
-				    fg_job = 0;
-				    return;
-			    } else {
-				    if(bgjobs == NULL || bgjobs->next==NULL)
-					    return;
-				    else {
-					    bgjobL * p= bgjobs->next;
-					    bgjobL * pre = bgjobs;		
-					    int job_num = 1;
-					    while(p!=NULL){
-						    if(p->pid == pid){
-							    //printf("[%d]   %s                    %s\n",job_num, "Done",p->cmd);
-							    //pre->next = p->next;
-							    //free(p->cmd);
-							    //free(p);
-							    p->state = DONE;
-							    return;
-						    }
-						    job_num++;
-						    if(p->next==NULL)
-							    break;
-						    pre = p;
-						    p=p->next;
+        pid_t pid = 1;
+        int status;
+        while(  pid >0 ){
+            pid = waitpid(-1, &status, WNOHANG );
+            if(WIFEXITED(status)){
+                if (pid == fg_job){ 
+                    fg_job = 0;
+                    return;
+                } else {
+                    if(bgjobs == NULL || bgjobs->next==NULL)
+                        return;
+                    else {
+                        bgjobL * p= bgjobs->next;
+                        bgjobL * pre = bgjobs;		
+                        int job_num = 1;
+                        while(p!=NULL){
+                            if(p->pid == pid){
+                                //printf("[%d]   %s                    %s\n",job_num, "Done",p->cmd);
+                                //pre->next = p->next;
+                                //free(p->cmd);
+                                //free(p);
+                                p->state = DONE;
+                                return;
+                            }
+                            job_num++;
+                            if(p->next==NULL)
+                                break;
+                            pre = p;
+                            p=p->next;
 
-					    }            
-				    }
+                        }            
+                    }
 
-			    }
-		    }
-	    } 
+                }
+            }
+        } 
 
     } 
-                                            
+
 }
 
